883f00a94d1a4fc38455b74c7ff44b3b5d67aa0c
erfanmhat <erfan.mh.at@gmail.com>
2021-09-27 02:18:00 +0330

    first commit

---
+++ app/src/main/java/ir/erfan_mh_at/test_ba_salam/MainActivity.kt
@@ -0,0 +1,11 @@
+package ir.erfan_mh_at.test_ba_salam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
