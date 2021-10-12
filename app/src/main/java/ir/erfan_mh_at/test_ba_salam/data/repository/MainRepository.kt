9207aa04ea7f9d965e57b679a3dc7e157e520dcd
erfanmhat <erfan.mh.at@gmail.com>
2021-10-12 13:45:29 +0330

    provide Retrofit instance in AppModule

--- app/src/main/java/ir/erfan_mh_at/test_ba_salam/data/repository/MainRepository.kt
+++ app/src/main/java/ir/erfan_mh_at/test_ba_salam/data/repository/MainRepository.kt
@@ -1,15 +1,14 @@
 package ir.erfan_mh_at.test_ba_salam.data.repository
 
-import ir.erfan_mh_at.test_ba_salam.data.remote.RetrofitInstance.Companion.BA_SALAM_API
 import ir.erfan_mh_at.test_ba_salam.common.Constants.QUERY_KEY_ANIMAL
 import ir.erfan_mh_at.test_ba_salam.common.Constants.QUERY_KEY_FLOWER
+import ir.erfan_mh_at.test_ba_salam.data.remote.BaSalamApi
 import javax.inject.Inject
 
-class MainRepository @Inject constructor() {
+class MainRepository @Inject constructor(
+    private val api: BaSalamApi
+) {
+    suspend fun getAnimals() = api.getAnimals(QUERY_KEY_ANIMAL)
 
-    suspend fun getAnimals() =
-        BA_SALAM_API.getAnimals(QUERY_KEY_ANIMAL)
-
-    suspend fun getFlowers() =
-        BA_SALAM_API.getFlowers(QUERY_KEY_FLOWER)
+    suspend fun getFlowers() = api.getFlowers(QUERY_KEY_FLOWER)
 }
--- app/src/main/java/ir/erfan_mh_at/test_ba_salam/di/AppModule.kt
+++ app/src/main/java/ir/erfan_mh_at/test_ba_salam/di/AppModule.kt
@@ -1,11 +1,41 @@
 package ir.erfan_mh_at.test_ba_salam.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import ir.erfan_mh_at.test_ba_salam.common.Constants.BASE_URL
+import ir.erfan_mh_at.test_ba_salam.data.remote.BaSalamApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor() =
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(interceptor: HttpLoggingInterceptor) = OkHttpClient.Builder()
+        .addInterceptor(interceptor)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideAPI(retrofit: Retrofit): BaSalamApi = retrofit.create(BaSalamApi::class.java)
 }
