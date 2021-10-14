994476dcec86219fb5d9a4e062be3c540067b23d
erfanmhat <erfan.mh.at@gmail.com>
2021-10-14 11:36:33 +0330

    update AnimalAndFlowerListState class

--- app/src/main/java/ir/erfan_mh_at/test_ba_salam/presentation/animal_and_flower_list/AnimalAndFlowerListState.kt
+++ app/src/main/java/ir/erfan_mh_at/test_ba_salam/presentation/animal_and_flower_list/AnimalAndFlowerListState.kt
@@ -2,14 +2,9 @@ package ir.erfan_mh_at.test_ba_salam.presentation.animal_and_flower_list
 
 import ir.erfan_mh_at.test_ba_salam.domain.model.AnimalAndFlower
 
-sealed class AnimalAndFlowerListState(
-    val animalAndFlowerList: List<AnimalAndFlower> = emptyList(),
-    val error: String = ""
-) {
+sealed class AnimalAndFlowerListState {
     object Empty : AnimalAndFlowerListState()
     object Loading : AnimalAndFlowerListState()
-    class Success(animalAndFlowerList: List<AnimalAndFlower>) :
-        AnimalAndFlowerListState(animalAndFlowerList)
-
-    class Error(errorMessage: String) : AnimalAndFlowerListState(error = errorMessage)
+    class Success(val animalAndFlowerList: List<AnimalAndFlower>) : AnimalAndFlowerListState()
+    class Error(val error: String) : AnimalAndFlowerListState()
 }
